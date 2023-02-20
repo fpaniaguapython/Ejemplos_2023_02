24588e1f49f2b61de9eec74a212fc6e50281bcee
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2023-02-20 20:53:23 +0100

    Create ejercicio_017_estructuras_de_datos.py

---
+++ ejercicio_017_estructuras_de_datos.py
@@ -0,0 +1,17 @@
+#Crear una lista de la compra con tres productos
+cesta = ["Brócoli","Huevos","Gaseosa"]
+print(cesta)
+
+#Solicitar al usuario un producto y agregarlo a la lista
+
+#Ordenar la lista
+
+#Mostrar el contenido de la lista
+
+#Solicitar al usuario un producto e indicar si está o no está en la lista
+
+#Solicitar al usuario un producto (que esté en la lista) y eliminarlo de la lista
+
+#Solicitar al usuario un producto e insertarlo en el centro de la lista
+
+#Solicitar al usuario un producto existente para sustituirlo por otro
